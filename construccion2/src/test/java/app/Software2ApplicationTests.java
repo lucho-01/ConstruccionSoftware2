303532ab5d2b326b9992d15362ad1336b203b664
lucho-01 <ricardomarzanluisjose@gmail.com>
2025-08-14 17:54:43 -0500

    initial commit

---
+++ construccion2/src/test/java/app/Software2ApplicationTests.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Software2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
