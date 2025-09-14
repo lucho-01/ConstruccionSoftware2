09649c11554494a39c0950a4a9fa767a91458897
LuisJosé <ricardomarzanluisjose@gmail.com>
2025-09-14 00:23:00 -0500

    some corrections

--- construccion2/src/main/java/app/domain/model/RegisterVisit.java
+++ construccion2/src/main/java/app/domain/model/RegisterVisit.java
@@ -6,36 +6,58 @@ public class RegisterVisit {
     private double temperature;
     private int pulse;
     private int oxygenLevel;
-    private String medication;
-    private String procedure;
-    private String testPerformed;
-    private String observations;
+    private Medications medications;
+    private Procedure procedure;
+    private DiagnosticAid diagnosticAid;
+    
+    
+	public String getPatientId() {
+		return patientId;
+	}
+	public void setPatientId(String patientId) {
+		this.patientId = patientId;
+	}
+	public String getBloodPressure() {
+		return bloodPressure;
+	}
+	public void setBloodPressure(String bloodPressure) {
+		this.bloodPressure = bloodPressure;
+	}
+	public double getTemperature() {
+		return temperature;
+	}
+	public void setTemperature(double temperature) {
+		this.temperature = temperature;
+	}
+	public int getPulse() {
+		return pulse;
+	}
+	public void setPulse(int pulse) {
+		this.pulse = pulse;
+	}
+	public int getOxygenLevel() {
+		return oxygenLevel;
+	}
+	public void setOxygenLevel(int oxygenLevel) {
+		this.oxygenLevel = oxygenLevel;
+	}
+	public Medications getMedications() {
+		return medications;
+	}
+	public void setMedications(Medications medications) {
+		this.medications = medications;
+	}
+	public Procedure getProcedure() {
+		return procedure;
+	}
+	public void setProcedure(Procedure procedure) {
+		this.procedure = procedure;
+	}
+	public DiagnosticAid getDiagnosticAid() {
+		return diagnosticAid;
+	}
+	public void setDiagnosticAid(DiagnosticAid diagnosticAid) {
+		this.diagnosticAid = diagnosticAid;
+	}
 
-    // Getters y Setters
-    public String getPatientId() { return patientId; }
-    public void setPatientId(String patientId) { this.patientId = patientId; }
-
-    public String getBloodPressure() { return bloodPressure; }
-    public void setBloodPressure(String bloodPressure) { this.bloodPressure = bloodPressure; }
-
-    public double getTemperature() { return temperature; }
-    public void setTemperature(double temperature) { this.temperature = temperature; }
-
-    public int getPulse() { return pulse; }
-    public void setPulse(int pulse) { this.pulse = pulse; }
-
-    public int getOxygenLevel() { return oxygenLevel; }
-    public void setOxygenLevel(int oxygenLevel) { this.oxygenLevel = oxygenLevel; }
-
-    public String getMedication() { return medication; }
-    public void setMedication(String medication) { this.medication = medication; }
-
-    public String getProcedure() { return procedure; }
-    public void setProcedure(String procedure) { this.procedure = procedure; }
-
-    public String getTestPerformed() { return testPerformed; }
-    public void setTestPerformed(String testPerformed) { this.testPerformed = testPerformed; }
-
-    public String getObservations() { return observations; }
-    public void setObservations(String observations) { this.observations = observations; }
 }
